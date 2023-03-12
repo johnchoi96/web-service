fe2bbd5f0689ad05080e82869fb6cfc945645819
John Choi <johnchoi1003@icloud.com>
2023-03-12 13:58:43 -0400

    Initial commit

---
+++ src/test/java/io/github/johnchoi96/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.johnchoi96.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
