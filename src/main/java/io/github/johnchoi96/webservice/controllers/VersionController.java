6ee02607f28bb78874f9e5811b5afb46a3023081
John Choi <johnchoi1003@icloud.com>
2023-03-21 02:26:40 -0400

    Issue #20 Fixed

--- src/main/java/io/github/johnchoi96/webservice/controllers/VersionController.java
+++ src/main/java/io/github/johnchoi96/webservice/controllers/VersionController.java
@@ -3,15 +3,12 @@ package io.github.johnchoi96.webservice.controllers;
 import io.github.johnchoi96.webservice.models.AppVersion;
 import io.github.johnchoi96.webservice.services.VersionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping(value = "/api/version")
@@ -21,9 +18,9 @@ public class VersionController {
     private VersionService versionService;
 
     @GetMapping(value = "/app", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AppVersion> getAppVersion(@RequestParam("bundleId") final String bundleId) throws IOException {
-        final AppVersion appVersion = versionService.getLatestVersion(bundleId);
-        if (appVersion == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    public ResponseEntity<AppVersion> getAppVersion(@RequestParam("appName") final String appName) {
+        final AppVersion appVersion = versionService.getLatestVersion(appName);
+        if (appVersion == null) return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(appVersion);
     }
 }
