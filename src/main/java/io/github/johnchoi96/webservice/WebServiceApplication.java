00e7ce9f1789e8267419a992d5ed7945c03ff6b8
John Choi <johnchoi1003@icloud.com>
2023-03-23 10:56:12 -0400

    Added cors config

--- src/main/java/io/github/johnchoi96/webservice/WebServiceApplication.java
+++ src/main/java/io/github/johnchoi96/webservice/WebServiceApplication.java
@@ -6,9 +6,6 @@ import io.github.johnchoi96.webservice.configs.VoaVersionConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({SendGridApiConfiguration.class, ConcealThisVersionConfiguration.class, VoaVersionConfiguration.class})
@@ -18,13 +15,4 @@ public class WebServiceApplication {
         SpringApplication.run(WebServiceApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://johnchoi96.github.io/");
-            }
-        };
-    }
 }
