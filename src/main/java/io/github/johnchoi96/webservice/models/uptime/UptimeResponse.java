a6d282aefe10c348d36f551ca989cb1cee97f93d
johnchoi96 <johnchoi1003@icloud.com>
2023-04-17 01:25:02 -0400

    Uptime json property changed to snake case

--- src/main/java/io/github/johnchoi96/webservice/models/uptime/UptimeResponse.java
+++ src/main/java/io/github/johnchoi96/webservice/models/uptime/UptimeResponse.java
@@ -1,5 +1,6 @@
 package io.github.johnchoi96.webservice.models.uptime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,8 +11,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class UptimeResponse {
+
+    @JsonProperty(value = "start_time")
     private String startTime;
 
+    @JsonProperty(value = "time_zone")
     private String timeZone;
 
     private UptimeObject uptime;
