33fb7e4f454f955fc38af15a2c93b566e90d2ca7
John Choi <johnchoi1003@icloud.com>
2023-04-21 10:29:28 -0400

    Test cases done

--- src/test/java/io/github/johnchoi96/webservice/services/EmailServiceTest.java
+++ src/test/java/io/github/johnchoi96/webservice/services/EmailServiceTest.java
@@ -4,6 +4,7 @@ import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import io.github.johnchoi96.webservice.models.EmailRequest;
 import io.github.johnchoi96.webservice.properties.api.SendGridApiProperties;
+import io.github.johnchoi96.webservice.properties.metadata.WebAppMetadataProperties;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -27,28 +30,41 @@ public class EmailServiceTest {
     @Mock
     private SendGridApiProperties sendGridApiProperties;
 
+    @Mock
+    private WebAppMetadataProperties webAppMetadataProperties;
+
     @InjectMocks
     @Autowired
     private EmailService emailService;
 
+    private final String DUMMY_APP_ID = "dummy-app-id";
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws NoSuchFieldException, IllegalAccessException {
         MockitoAnnotations.openMocks(this);
 
         doReturn("API_KEY").when(sendGridApiProperties).getApiKey();
+        final Field field = EmailService.class.getDeclaredField("allowedAppIds");
+        field.setAccessible(true);
+        field.set(emailService, List.of(DUMMY_APP_ID));
     }
 
     @Test
     @Disabled
     void testSendEmail() throws IOException {
         doReturn(getDummyResponse(200)).when(sendGrid).api(any());
-        assertTrue(emailService.sendEmailForContactMe(getCompleteRequest(), "johnchoi-portfolio"));
+        assertTrue(emailService.sendEmailForContactMe(getCompleteRequest(), DUMMY_APP_ID));
     }
 
     @Test
     void testSendEmailWithBadResponse() throws IOException {
         doReturn(getDummyResponse(400)).when(sendGrid).api(any());
-        assertFalse(emailService.sendEmailForContactMe(getCompleteRequest(), "johnchoi-portfolio"));
+        assertFalse(emailService.sendEmailForContactMe(getCompleteRequest(), DUMMY_APP_ID));
+    }
+
+    @Test
+    void testInvalidAppId() {
+        assertFalse(emailService.sendEmailForContactMe(getCompleteRequest(), "invalid-app-id"));
     }
 
     private Response getDummyResponse(final int statusCode) {
--- src/test/java/io/github/johnchoi96/webservice/services/VersionServiceTest.java
+++ src/test/java/io/github/johnchoi96/webservice/services/VersionServiceTest.java
@@ -1,43 +1,64 @@
 package io.github.johnchoi96.webservice.services;
 
+import io.github.johnchoi96.webservice.properties.metadata.ConcealThisVersionProperties;
+import io.github.johnchoi96.webservice.properties.metadata.VoaVersionProperties;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 
 public class VersionServiceTest {
 
+    @Mock
     @Autowired
-    @Spy
-    private VersionService service;
+    private ConcealThisVersionProperties concealThisVersionProperties;
+
+    @Mock
+    @Autowired
+    private VoaVersionProperties voaVersionProperties;
+
+    @Autowired
+    @InjectMocks
+    private VersionService versionService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+
+        doReturn("conceal-version").when(concealThisVersionProperties).getVersion();
+        doReturn("voa-version").when(voaVersionProperties).getVersion();
     }
-
-
+    
     @Test
     void testGetLatestVersionWithValidBundleId() {
-        final String versionString = "valid-version";
-        doReturn(versionString).when(service).getAppVersionFromFile(any());
+        final String versionString = "voa-version";
         final String appName = "voa";
-        var appVersion = service.getLatestVersion(appName);
+        var appVersion = versionService.getLatestVersion(appName);
         assertEquals(versionString, appVersion.getVersion());
     }
 
     @Test
     void testGetLatestVersionWithInvalidBundleId() {
-        final String versionString = "valid-version";
-        doReturn(versionString).when(service).getAppVersionFromFile(any());
         final String appName = "invalid-bundle-id";
-        var appVersion = service.getLatestVersion(appName);
+        var appVersion = versionService.getLatestVersion(appName);
         assertNull(appVersion);
+    }
+
+    @Test
+    void testGetConcealAppVersionFromFile() {
+        final String expected = "conceal-version";
+        assertEquals(expected, versionService.getAppVersionFromFile(VersionService.AppNames.ConcealThis));
+    }
+
+    @Test
+    void testGetVoaAppVersionFromFile() {
+        final String expected = "voa-version";
+        assertEquals(expected, versionService.getAppVersionFromFile(VersionService.AppNames.VOA));
     }
 }
