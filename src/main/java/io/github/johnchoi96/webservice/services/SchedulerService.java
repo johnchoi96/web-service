06df3a182ede9aab5aa93f85fa07a674a03bead1
John Choi <johnchoi1003@icloud.com>
2023-04-25 03:31:13 -0400

    Added condition to scheduler

--- src/main/java/io/github/johnchoi96/webservice/services/SchedulerService.java
+++ src/main/java/io/github/johnchoi96/webservice/services/SchedulerService.java
@@ -3,6 +3,7 @@ package io.github.johnchoi96.webservice.services;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +11,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class SchedulerService {
 
+    @Value("${scheduler.enabled}")
+    private boolean schedulerEnabled;
+
     @Autowired
     private PetfinderService petfinderService;
-
 
     // Sundays, Wednesdays, and Fridays at 12pm in EST
     @Scheduled(cron = "0 0 12 ? * SUN,WED,FRI", zone = "America/New_York")
     public void findDogsNear43235() throws JsonProcessingException {
-        log.info("Starting job for findDogsNear43235()");
-        petfinderService.findFilteredDogsAndReport();
-        log.info("Finished job for findDogsNear43235()");
+        if (schedulerEnabled) {
+            log.info("Starting job for findDogsNear43235()");
+            petfinderService.findFilteredDogsAndReport();
+            log.info("Finished job for findDogsNear43235()");
+        }
     }
 }
