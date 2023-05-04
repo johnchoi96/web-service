faa2dc3ef75aa7770c79aa82e5bd6cf81a61885f
John Choi <johnchoi1003@icloud.com>
2023-05-04 03:03:02 -0400

    Added root controller

---
+++ src/main/java/io/github/johnchoi96/webservice/controllers/RootController.java
@@ -0,0 +1,20 @@
+package io.github.johnchoi96.webservice.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@Slf4j
+public class RootController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> root() {
+        log.info("GET /");
+        return ResponseEntity.ok("You've reached johnchoi96's web service");
+    }
+}
