294502ac091f15733f65e389d51937e3e0341f06
John Choi <johnchoi1003@icloud.com>
2023-12-02 00:22:55 -0500

    Reverting changes

--- src/main/java/io/github/johnchoi96/webservice/properties/api/FirebaseProperties.java
+++ src/main/java/io/github/johnchoi96/webservice/properties/api/FirebaseProperties.java
@@ -3,10 +3,11 @@ package io.github.johnchoi96.webservice.properties.api;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
 
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "gcp.firebase")
 public class FirebaseProperties {
-    private String serviceAccountPath;
+    private Resource serviceAccount;
 }
