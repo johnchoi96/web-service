e4171b3dc5f18613b52ee58e393ec69708fa3285
John Choi <johnchoi1003@icloud.com>
2024-10-06 11:49:55 -0400

    Sets CFBWeek to final if weekly sunday CFB report triggers

--- src/main/java/io/github/johnchoi96/webservice/models/cfb/api_response/CfbUpsetMatchResponse.java
+++ src/main/java/io/github/johnchoi96/webservice/models/cfb/api_response/CfbUpsetMatchResponse.java
@@ -4,10 +4,12 @@ import io.github.johnchoi96.webservice.entities.cfb.CfbUpsetEntity;
 import io.github.johnchoi96.webservice.entities.cfb.CfbWeekSummaryEntity;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @Builder
 public class CfbUpsetMatchResponse {
     private List<CfbUpsetEntity> upsetGames;
