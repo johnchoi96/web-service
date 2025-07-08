2b003a37a75ff911249bef2ffff9647dba929846
John Choi <johnchoi1003@icloud.com>
2025-07-08 01:25:31 -0500

    Created endpoint to enable sending notifications on demand

--- src/main/java/io/github/johnchoi96/webservice/controllers/FCMController.java
+++ src/main/java/io/github/johnchoi96/webservice/controllers/FCMController.java
@@ -3,19 +3,25 @@ package io.github.johnchoi96.webservice.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import io.github.johnchoi96.webservice.models.firebase.fcm.FCMTopic;
+import io.github.johnchoi96.webservice.models.firebase.fcm.SendNotificationPayload;
 import io.github.johnchoi96.webservice.services.CfbService;
 import io.github.johnchoi96.webservice.services.EmailService;
 import io.github.johnchoi96.webservice.services.FCMService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -90,6 +96,43 @@ public class FCMController {
             cfbService.triggerUpsetReport();
             return ResponseEntity.ok().build();
         } catch (JsonProcessingException | FirebaseMessagingException e) {
+            emailService.notifyException(e);
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @PostMapping(value = "/send-notification", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Sends a notification to all if topic isn't specified.",
+            security = {@SecurityRequirement(name = "basicAuth")},
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Notification successfully sent"
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "400", description = "Possibly invalid topic or payload"),
+                    @ApiResponse(responseCode = "500", description = "Something went wrong")
+            }
+    )
+    public ResponseEntity<?> sendNotification(
+            @RequestParam(required = false, value = "topic") String topicValue,
+            @Valid @RequestBody final SendNotificationPayload payload) {
+        log.info("POST /api/fcm/send-notification");
+        FCMTopic topic;
+        if (topicValue == null) {
+            topic = FCMTopic.ALL;
+        } else {
+            topic = FCMTopic.getTopic(topicValue);
+            if (topic == null) {
+                return ResponseEntity.badRequest().body("Invalid topic type");
+            }
+        }
+        try {
+            fcmService.sendNotification(topic, payload.getTitle(), payload.getSubtitle(),
+                    new StringBuilder(payload.getMessage()), payload.isHtml(), false);
+            return ResponseEntity.ok().build();
+        } catch (final FirebaseMessagingException e) {
             emailService.notifyException(e);
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
