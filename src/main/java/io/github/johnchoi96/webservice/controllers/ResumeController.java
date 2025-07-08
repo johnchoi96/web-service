eb595f80101911a900aa132f5f99d6698928ed4b
John Choi <johnchoi1003@icloud.com>
2025-07-08 00:32:06 -0500

    Enabled Spring Security and replaced all admin key request params

--- src/main/java/io/github/johnchoi96/webservice/controllers/ResumeController.java
+++ src/main/java/io/github/johnchoi96/webservice/controllers/ResumeController.java
@@ -1,12 +1,11 @@
 package io.github.johnchoi96.webservice.controllers;
 
-import io.github.johnchoi96.webservice.properties.adminkeys.AdminKeysProperties;
 import io.github.johnchoi96.webservice.services.ResumeService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.http.HttpStatusCode;
 
 import java.io.IOException;
 
@@ -30,8 +28,6 @@ import java.io.IOException;
 public class ResumeController {
 
     private final ResumeService resumeService;
-
-    private final AdminKeysProperties adminKeysProperties;
 
     @Operation(
             summary = "Download Resume as PDF",
@@ -70,6 +66,7 @@ public class ResumeController {
     @Operation(
             summary = "Triggers resume refresh on S3 bucket",
             description = "Fetches the resume document from Google Docs and uploads to S3 bucket.",
+            security = {@SecurityRequirement(name = "basicAuth")},
             responses = {
                     @ApiResponse(
                             responseCode = "200",
@@ -80,11 +77,8 @@ public class ResumeController {
             }
     )
     @PutMapping(value = "/refresh")
-    public ResponseEntity<?> refreshResume(@Parameter(description = "admin key") @RequestParam final String key) {
+    public ResponseEntity<?> refreshResume() {
         log.info("PUT /api/resume/refresh");
-        if (!adminKeysProperties.getAdminKey().equals(key)) {
-            return ResponseEntity.status(HttpStatusCode.UNAUTHORIZED).body("Invalid admin key");
-        }
         try {
             resumeService.refreshResume();
             return ResponseEntity.ok().build();
